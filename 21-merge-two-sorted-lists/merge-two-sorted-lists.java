7f08e99e33310e7ebb79867c075c0d2ac4901f8d
Vishnu <vishnuprasadn88@gmail.com>
2025-08-06 17:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (75.60%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,34 +10,68 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-      ListNode temp1 = list1;
-      ListNode temp2 = list2;
+//       ListNode temp1 = list1;
+//       ListNode temp2 = list2;
 
-      ListNode newNode = new ListNode(100);
-        ListNode temp = newNode;
+//       ListNode newNode = new ListNode(100);
+//         ListNode temp = newNode;
+
+// while(temp1!=null && temp2!=null){
+//     if(temp1.val < temp2.val){
+//         ListNode a = new ListNode(temp1.val);
+//         temp.next = a;
+//         temp = a;
+//         temp1 = temp1.next;
+//       }else{
+//         ListNode a = new ListNode(temp2.val);
+//         temp.next = a;
+//         temp = a;
+//         temp2 = temp2.next;
+//       }
+
+// }
+
+// if(temp1==null){
+//     temp.next = temp2;
+// }
+
+// if(temp2 == null){
+//     temp.next = temp1;
+// }
+//       return newNode.next;
+
+//uses extra space ^^^^^
+
+ListNode temp1 = list1;
+ListNode temp2 = list2;
+
+ListNode newNode = new ListNode(1000); //Dummy node;
+ListNode temp = newNode;
 
 while(temp1!=null && temp2!=null){
-    if(temp1.val < temp2.val){
-        ListNode a = new ListNode(temp1.val);
-        temp.next = a;
-        temp = a;
+    if(temp1.val<temp2.val){
+        temp.next = temp1;
+        temp = temp1;
         temp1 = temp1.next;
-      }else{
-        ListNode a = new ListNode(temp2.val);
-        temp.next = a;
-        temp = a;
-        temp2 = temp2.next;
-      }
+    }else{
+        temp.next = temp2;
+        temp  =temp2;
+        temp2=  temp2.next;
 
+    }
 }
 
-if(temp1==null){
+if(temp1 == null){
     temp.next = temp2;
+
 }
 
 if(temp2 == null){
     temp.next = temp1;
 }
-      return newNode.next;
+
+return newNode.next;
+
+
     }
 }
