3f46887de9ba11ac46bfc708ef5934bb27c809c8
Vishnu <vishnuprasadn88@gmail.com>
2025-08-09 20:46:08 +0530

    Time: 1 ms (99.87%) | Memory: 53.4 MB (20.84%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,48 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        // int first = 0;
-        // int last = n - 1;
-        
-
-        // while(first<=last){
-        //     if(nums[first] >= nums[last]){
-        //          return nums[first++];
-        //     }else{
-        //         return nums[last--];
+       
+        // for(int i = 0;i<n;i++){
+        //     int count = 0;
+        //     for(int j =0;j<n;j++){
+        //         if(nums[j] == nums[i]){
+        //             count++;
+        //         }
         //     }
+        //     if(count >  n/2)
+        //         return nums[i];
         // }
 
+        // return -1;
 
+        int count = 0;
+        int majEle = Integer.MAX_VALUE;
 
-        // while(first<= last){
-        //     if(nums[first] > times){
-        //         int temp = nums[first];
-        //         first++;
-        //         return temp;
-        //     }else if(nums[first] == times) {
-        //         return nums[first] ;
-        //     }
-        //     else{
-        //         int temp = nums[last];
-        //         last--;
-        //         return temp;
-        //     }
-        // }
-
-        // return 0;
-
-        for(int i = 0;i<n;i++){
-            int count = 0;
-            for(int j =0;j<n;j++){
-                if(nums[j] == nums[i]){
-                    count++;
-                }
-            }
-            if(count >  n/2)
-                return nums[i];
+    for(int i = 0;i<n;i++){
+        if(nums[i] == majEle){
+            count++;
+        }else if(count == 0){
+            majEle = nums[i];
+            count = 1;
+        }else{
+           count--;
         }
-
-        return -1;
+    }
+    return majEle;
     }
 }
