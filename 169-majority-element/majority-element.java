a068466c782cf678f68e62291215fb6f07bc80a3
Vishnu <vishnuprasadn88@gmail.com>
2025-08-10 18:53:02 +0530

    Time: 1 ms (99.87%) | Memory: 53.4 MB (28.99%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -15,19 +15,21 @@ class Solution {
 
         // return -1;
 
-        int count = 0;
-        int majEle = Integer.MAX_VALUE;
+        //BOYER - MOORE's ALGO
 
-    for(int i = 0;i<n;i++){
-        if(nums[i] == majEle){
-            count++;
-        }else if(count == 0){
-            majEle = nums[i];
-            count = 1;
-        }else{
-           count--;
+        int count = 1;
+        int majEle = nums[0];
+
+        for(int i = 1;i<n;i++){
+            if(nums[i] == majEle){
+                count++;
+            }else if(count == 0){
+                majEle = nums[i];
+                count = 1;
+            }else{
+                count--;
+            }
         }
-    }
-    return majEle;
+        return majEle;
     }
 }
