6ea92bb37d688c5056e6aef4e60a696d2e9efab3
Vishnu <vishnuprasadn88@gmail.com>
2025-08-18 17:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (30.75%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,68 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // int count_0 = 0;
-        // int count_1 = 0;
-        // int  count_2 = 0;
-        //  for(int i : nums){
-        //     if( i == 0){
-        //         count_0++;
-        //     }else if(i == 1){
-        //         count_1++;
-        //     }else if(i == 2){
-        //         count_2++;
-        //     }
-        //  }
-
-        //  int k = 0;
-        //  while(count_0>0){
-        //     nums[k] = 0;
-        //     k++;
-        //     count_0--;
-        //  }
-
-        //  while(count_1 > 0){
-        //     nums[k] = 1;
-        //     k++;
-        //     count_1--;
-        //  }
-
-        //   while(count_2 > 0){
-        //     nums[k] = 2;
-        //     k++;
-        //     count_2--;
-        //  }
-
-        /* DUTCH 
-            NATIONAL
-                FLAG
-                    ALGORITHM 
-                             */
-                             
         int n = nums.length;
-        int low = 0;
-        int mid = 0;
-        int high = n -1;
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
 
-        while(mid<=high){
-            if(nums[mid] == 0){
-                swap(nums,low,mid);
-                mid++;
-                low++;   
-            }else if(nums[mid] == 1){
-                mid++;
-
-            }else {
-                swap(nums , mid, high);
-                high--;
-            }
+        for(int i = 0;i<n;i++){
+            if(nums[i]== 0) count0++;
+            else if(nums[i] == 1) count1++;
+            else count2++;
         }
 
+        for(int i = 0;i<count0;i++){
+            nums[i] = 0;
+        }
         
-    }
+        for(int i = count0 ; i<count0 + count1;i++){
+            nums[i] = 1;
+        }
 
-    public void swap(int[] nums,int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        for(int i = count0 + count1;i<n;i++){
+            nums[i] = 2;
+        }
     }
 }
