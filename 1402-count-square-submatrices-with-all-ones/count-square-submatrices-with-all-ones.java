36eed97517503659e2d678e1dcfb398eaf79f3aa
Vishnu <vishnuprasadn88@gmail.com>
2025-08-21 00:05:42 +0530

    Time: 6 ms (75.00%) | Memory: 54.9 MB (85.77%) - LeetSync

---
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int[][] dp = new int[rows][cols];  // dp[i][j] = largest square ending at (i, j)
+        int total = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        // First row or first column can only form 1x1 square
+                        dp[i][j] = 1;
+                    } else {
+                        // Take minimum of three neighbors and add 1
+                        dp[i][j] = 1 + Math.min(dp[i - 1][j],
+                                        Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                    }
+                    total += dp[i][j];
+                }
+            }
+        }
+
+        return total;
+    }
+}
