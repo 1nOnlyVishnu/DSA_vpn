bdf19fe723524d38c663d45974e40dbb62de6527
Vishnu <vishnuprasadn88@gmail.com>
2025-10-23 22:56:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (70.06%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,21 +1,59 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int l = 0, r = nums.length - 1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (nums[m] == target) return m;
+    private int BS(int[] nums,int index1,int index2,int target){
+        while(index1<=index2){
+            int mid = index1 + (index2 - index1)/2;
 
+            if(target == nums[mid]){
+                return mid;
+            }else if(target<nums[mid]){
+                index2 = mid - 1;
+            }else{
+                index1 = mid + 1;
+            }
+        }
 
-            if (nums[l] <= nums[m]) {
-                if (nums[l] <= target && target < nums[m]) r = m - 1;
-                else l = m + 1;
-            } 
-    
-            else {
-                if (nums[m] < target && target <= nums[r]) l = m + 1;
-                else r = m - 1;
+        return -1;
+    }
+
+    private int findMinIndex(int[] nums,int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int n = nums.length;
+
+        
+
+        if(start == end) return start;
+
+        if(nums[start] <= nums[end]) return start;
+
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            int next = (mid + 1)%n;
+            int prev = (mid + n - 1) %n;
+
+            if(nums[mid] <= nums[prev] && nums[mid] <= nums[next]){
+                return mid;
+            }else if(nums[0] <= nums[mid]){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
             }
         }
         return -1;
+    }
+    public int search(int[] nums, int target) {
+        int start = 0;
+        if(nums[start] == target) return start;
+        int n =nums.length - 1;
+        int index = findMinIndex(nums,target);
+
+        int num1 = BS(nums,index,n,target);
+        int num2 = BS(nums,0,index-1,target);
+
+        if(num1>0){
+            return num1;
+        }
+
+        return num2;
     }
 }
