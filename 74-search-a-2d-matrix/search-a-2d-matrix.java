79b015bbb9a34fdf621407abfbbf18397eca528f
Vishnu <vishnuprasadn88@gmail.com>
2025-11-03 23:49:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (6.84%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,68 +1,21 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix(int[][] arr, int target) {
+        int m = arr.length;
+        int n = arr[0].length;
+        int i = 0;
 
-        // int m = matrix.length;
-        // for(int i = 0;i<m;i++){
-        //     int n = matrix[i].length;
-        //     for(int j = 0;j<n;j++){
-        //         if(matrix[i][j] == target){
-        //             return true;
-        //         }
-        //     }
-        // }
+        int j = n -1;
 
-        // return false;
-        // int m = matrix.length;
-        
-
-        // for(int i =0 ;i<m;i++){
-        //     int n = matrix[i].length;
-        //     if(matrix[i][0] <= target && target<=matrix[i][n-1]){
-        //         return BinarySearch(matrix[i],target);
-        //     }
-        // }
-        // return false; 
-
-        if(matrix.length == 0) return false;
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        int low = 0;
-        int high = (n*m)-1;
-
-        while(low<=high){
-            int mid =low+ (high-low)/2;
-            int row = mid/n;
-            int col = mid%n;
-
-            if(matrix[row][col] == target){
+        while(i>=0 && i <m && j>=0 && j< n){
+            if(arr[i][j] == target){
                 return true;
-            }else if (matrix[row][col]>target){
-                high = mid-1;
-            }else{
-                low = mid +1;
+            }else if(arr[i][j] > target){
+                j--;
+            }else if(arr[i][j] < target){
+                i++;
             }
         }
 
         return false;
     }
-
-    // public boolean BinarySearch(int[] arr,int target){
-    //     int low = 0;
-    //     int high =  arr.length - 1;
-        
-
-    // while(low<=high){
-    //     int mid = (low+high)/2;
-    //     if(target == arr[mid]){
-    //             return true;
-    //     }else if(target>arr[mid]){
-    //         low = mid+1;
-    //     }else{
-    //         high = mid - 1;
-    //     }
-    // }
-
-    // return false;    
-    // }
 }
