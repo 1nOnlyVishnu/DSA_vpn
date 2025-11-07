bb9e76dd5f6b1bca0367b23a8265178c9c616686
Vishnu <vishnuprasadn88@gmail.com>
2025-11-07 15:41:18 +0530

    Time: 62 ms (37.35%) | Memory: 47 MB (55.23%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,44 +1,89 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
+    //     int n = nums.length;
 
-        int sum = 0;
-        for(int num:nums){
-            sum = sum + num;
-        }
+    //     int sum = 0;
+    //     for(int num:nums){
+    //         sum = sum + num;
+    //     }
 
-        if(sum%2 != 0){
-            return false;
-        }else{
-           return SubsetSum(nums,sum/2);
-        }
+    //     if(sum%2 != 0){
+    //         return false;
+    //     }else{
+    //        return SubsetSum(nums,sum/2);
+    //     }
+    // }
+
+    // public boolean SubsetSum(int[] nums,int sum){
+    //     int n = nums.length;
+    //     boolean[][] t = new boolean[n+1][sum+1];
+
+    //     for(int i = 0;i<n+1;i++){
+    //         for(int j=0;j<sum+1;j++){
+    //             if(i == 0){
+    //                 t[i][j] = false;
+    //             }
+
+    //             if(j==0){
+    //                 t[i][j] = true;
+    //             }
+    //         }
+    //     }
+
+    //     for(int i=1;i<n+1;i++){
+    //         for(int j = 1;j<sum+1;j++){
+    //             if(nums[i-1] <= j){
+    //                 t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
+    //             }else{
+    //                 t[i][j] = t[i-1][j];
+    //             }
+    //         }
+
+    //     }
+
+    //     return t[n][sum];
+
+    int n = nums.length;
+    int sum = 0;
+
+    for(int num:nums){
+        sum+=num;
     }
 
-    public boolean SubsetSum(int[] nums,int sum){
+    if(sum%2 != 0){
+        return false;
+    }
+
+    return subsetSum(nums,sum/2);
+    
+    }
+
+    public boolean subsetSum(int[] nums,int sum){
         int n = nums.length;
+
         boolean[][] t = new boolean[n+1][sum+1];
 
-        for(int i = 0;i<n+1;i++){
+        for(int i=0;i<n+1;i++){
             for(int j=0;j<sum+1;j++){
                 if(i == 0){
                     t[i][j] = false;
                 }
 
-                if(j==0){
+                if(j== 0){
                     t[i][j] = true;
                 }
             }
         }
 
+
         for(int i=1;i<n+1;i++){
-            for(int j = 1;j<sum+1;j++){
+            for(int j=1;j<sum+1;j++){
                 if(nums[i-1] <= j){
                     t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
                 }else{
                     t[i][j] = t[i-1][j];
                 }
             }
-
         }
 
         return t[n][sum];
