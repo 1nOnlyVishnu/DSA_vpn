a9ceeebeffbbfff4e06eb45f130c0173d87e552a
Vishnu <vishnuprasadn88@gmail.com>
2025-11-11 20:10:48 +0530

    Time: 17 ms (28.16%) | Memory: 56.5 MB (11.18%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,28 +1,35 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
+       int n = coins.length;
+       int[][] t = new int[n+1][amount+1];
 
-        int[][] t = new int[n+1][amount+1];
+       t[0][0] = 0;
 
-        for(int i = 0;i<=n;i++){
-            t[i][0] = 1;
-        }
-
-        for(int j = 1;j<=amount;j++){
-            t[0][j] = 0;
-        }
-
-        for(int i = 1;i<=n;i++){
-            for(int j =1;j<=amount;j++){
-                if(coins[i-1] <= j){
-                    t[i][j] = t[i-1][j] + t[i][j- coins[i-1]];
+       for(int i = 0;i<n+1;i++){
+            for(int j = 0;j<amount+1;j++){
+                if(i == 0){
+                    t[i][j] = 0;
                 }
-                else{
-                    t[i][j] = t[i-1][j];
+
+                if(j == 0){
+                    t[i][j] = 1;
                 }
             }
-        }
+       }
 
-        return t[n][amount];
+       for(int i = 1;i<n+1;i++){
+        for(int j =1;j<amount+1;j++){
+            if(coins[i-1]<=j){
+                t[i][j] = t[i-1][j] + t[i][j-coins[i-1]];
+            }
+            else{
+                t[i][j] = t[i-1][j];
+            }
+        }
+       }
+
+       return t[n][amount];
+
+
     }
 }
