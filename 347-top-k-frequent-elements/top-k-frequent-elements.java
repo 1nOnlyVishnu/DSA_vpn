1c24b04b51da58634004326d17e262e7a5100885
Vishnu <vishnuprasadn88@gmail.com>
2026-01-07 17:08:12 +0530

    Time: 41 ms (5.39%) | Memory: 47.8 MB (23.84%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,38 +1,26 @@
-
-class Pair {
-    int freq;
-    int num;
-
-    Pair(int freq, int num) {
-        this.freq = freq;
-        this.num = num;
-    }
-}
-
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
 
-        
-        PriorityQueue<Pair> heap = new PriorityQueue<>((a, b) -> a.freq - b.freq);
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        List<Integer> values = new ArrayList<>(map.values());
 
-      
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            heap.add(new Pair(entry.getValue(), entry.getKey()));
-            if (heap.size() > k) {
-                heap.poll();
-            }
-        }
-
-    
         int[] result = new int[k];
-        int i = 0;
-        while (!heap.isEmpty()) {
-            result[i++] = heap.poll().num;
+
+        for(int i=0;i<k;i++){
+            int maxVal = Collections.max(values);
+            int index = values.indexOf(maxVal);
+
+            int element = keys.get(index);
+
+            result[i] = element;
+
+            values.remove(index);
+            keys.remove(index);
         }
 
         return result;
