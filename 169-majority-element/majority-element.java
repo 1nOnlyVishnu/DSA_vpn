73cc3811aac529c78bbb9e5bbc507e7b65a51425
Vishnu <vishnuprasadn88@gmail.com>
2026-05-26 19:20:41 +0530

    Time: 17 ms (15.63%) | Memory: 52.6 MB (80.74%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,35 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
         int n = nums.length;
-       
-        // for(int i = 0;i<n;i++){
-        //     int count = 0;
-        //     for(int j =0;j<n;j++){
-        //         if(nums[j] == nums[i]){
-        //             count++;
-        //         }
-        //     }
-        //     if(count >  n/2)
-        //         return nums[i];
-        // }
 
-        // return -1;
+        for ( int i = 0 ; i < nums.length; i++ ){
+            map.put(nums[i], map.getOrDefault(nums[i] , 0) + 1);
 
-        //BOYER - MOORE's ALGO
-
-        int count = 1;
-        int majEle = nums[0];
-
-        for(int i = 1;i<n;i++){
-            if(nums[i] == majEle){
-                count++;
-            }else if(count == 0){
-                majEle = nums[i];
-                count = 1;
-            }else{
-                count--;
+            if (map.get(nums[i]) > n / 2) {
+                return nums[i];
             }
         }
-        return majEle;
+return -1;
+
     }
 }
