2c5aea34259d839f06a9ae4977d8cf451fdf121f
Vishnu <vishnuprasadn88@gmail.com>
2026-06-04 14:02:21 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (26.72%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,59 +1,52 @@
 class Solution {
-    private int BS(int[] nums,int index1,int index2,int target){
-        while(index1<=index2){
-            int mid = index1 + (index2 - index1)/2;
 
-            if(target == nums[mid]){
-                return mid;
-            }else if(target<nums[mid]){
-                index2 = mid - 1;
-            }else{
-                index1 = mid + 1;
+    private int findMinEleIndex(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
             }
         }
 
-        return -1;
+        return start;
     }
 
-    private int findMinIndex(int[] nums,int target){
-        int start = 0;
-        int end = nums.length - 1;
-        int n = nums.length;
+    private int binarySearchHelper(int[] nums, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-        
-
-        if(start == end) return start;
-
-        if(nums[start] <= nums[end]) return start;
-
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            int next = (mid + 1)%n;
-            int prev = (mid + n - 1) %n;
-
-            if(nums[mid] <= nums[prev] && nums[mid] <= nums[next]){
+            if (nums[mid] == target) {
                 return mid;
-            }else if(nums[0] <= nums[mid]){
+            } else if (nums[mid] < target) {
                 start = mid + 1;
-            }else{
+            } else {
                 end = mid - 1;
             }
         }
+
         return -1;
     }
+
     public int search(int[] nums, int target) {
-        int start = 0;
-        if(nums[start] == target) return start;
-        int n =nums.length - 1;
-        int index = findMinIndex(nums,target);
+        int n = nums.length;
 
-        int num1 = BS(nums,index,n,target);
-        int num2 = BS(nums,0,index-1,target);
-
-        if(num1>0){
-            return num1;
+        if (n == 0) {
+            return -1;
         }
 
-        return num2;
+        int pivot = findMinEleIndex(nums);
+
+        int left = binarySearchHelper(nums, 0, pivot - 1, target);
+        if (left != -1) {
+            return left;
+        }
+
+        return binarySearchHelper(nums, pivot, n - 1, target);
     }
 }
