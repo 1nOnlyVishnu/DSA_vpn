d01d87971e891c8a80bc65d64bc5c3bc2ff6e705
Vishnu <vishnuprasadn88@gmail.com>
2026-07-03 20:40:45 +0530

    Time: 424 ms (98.28%) | Memory: 22.2 MB (53.95%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,44 +1,43 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+    def pair_sum_sorted(self,  nums: list[int], start: int, target: int) -> list[int]:
+        left = start
+        right = len(nums) - 1
+        pairs = []
+
+        while left < right:
+            sum = nums[left] + nums[right]
+
+            if sum == target:
+                pairs.append([nums[left], nums[right]])
+                left += 1
+
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+            elif sum < target:
+                left += 1
+            else :
+                right -= 1
+        return pairs
+
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        n = len(nums)
         nums.sort()
-        triplets = []
+        triplets = [] 
 
-        for i in range(len(nums) - 2):
-
+        for i in range(n):
             if nums[i] > 0:
                 break
-
-            if i > 0 and nums[i] == nums[i - 1]:
+            
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            pairs = self.pair_sum_sorted_all_pairs(nums, i + 1, -nums[i])
+            pairs = self.pair_sum_sorted(nums, i + 1, -nums[i])
 
             for pair in pairs:
                 triplets.append([nums[i]] + pair)
 
         return triplets
 
-
-    def pair_sum_sorted_all_pairs(self, nums: List[int], start: int, target: int) -> List[List[int]]:
-        pairs = []
-        left, right = start, len(nums) - 1
-
-        while left < right:
-            curr = nums[left] + nums[right]
-
-            if curr == target:
-                pairs.append([nums[left], nums[right]])
-                left += 1
-                right -= 1
-
-                while left < right and nums[left] == nums[left - 1]:
-                    left += 1
-                while left < right and nums[right] == nums[right + 1]:
-                    right -= 1
-
-            elif curr < target:
-                left += 1
-            else:
-                right -= 1
-
-        return pairs
+                
+        
