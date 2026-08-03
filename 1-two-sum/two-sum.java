372f58787f5f4bd7527d45f8753409c49c34b6eb
Vishnu <vishnuprasadn88@gmail.com>
2026-08-03 21:34:45 +0530

    Time: 2 ms (99.33%) | Memory: 47.1 MB (59.47%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,28 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // target ,  nums array as input
-        // find two val that add up to target
-        // return their indices
-        // x + y = target
-        // use value from nums as x, then the equation will become something like 2 + y = 9 
-        // y = 9 - 2;
-        // y = target - nums[i];
-        // O(1) == set and map
-        // if i use set == > keys (individual values) but for this problem i need to return the indices...
-        // hashmap ==>  nums[i] , i
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
 
-        HashMap<Integer,Integer> freqMap = new HashMap<>();
-
-        for ( int i = 0; i < nums.length; i++ ){
-            int y = target - nums[i];
-            if(freqMap.containsKey(y)){
-                return new int[] {freqMap.get(y),i};
+            if(!map.containsKey(complement)){
+                map.put(nums[i] , i);
+            }else{
+                return new int[] {map.get(complement) , i};
             }
-
-            freqMap.put(nums[i] , i);
         }
 
-        return new int[] {};
+        return new int[] {-1,-1};
     }
 }
