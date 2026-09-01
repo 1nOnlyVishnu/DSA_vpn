19284bd99e265b193ac6c3a6b5f9a74f2c0539bf
Vishnu <vishnuprasadn88@gmail.com>
2026-09-01 17:28:24 +0530

    Time: 274 ms (68.54%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,10 +1,6 @@
 # Write your MySQL query statement below
-SELECT
-    *
-FROM
-    Cinema
-WHERE
-    description <> 'boring'
-    AND id % 2 = 1
-ORDER BY
-    rating DESC;
+SELECT *
+FROM Cinema
+WHERE MOD(id , 2) = 1
+AND description != 'boring'
+ORDER BY rating DESC;
